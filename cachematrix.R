b7f8f35f31fbe15ca501d0967db3566763e4a50c
administrator <administrator@lubuntu-14.10.64>
2015-05-24 11:15:52 +0200

    added description to cacheSolve

--- cachematrix.R
+++ cachematrix.R
@@ -9,17 +9,22 @@
 #  - computes the inverse of an object created with makeCacheMatrix
 #  - prints "getting cached data" if the inverse was already computed before
 # -- EXAMPLE WALKTHROUGH --
-# 1) create object matr containing a 2x2 matrix with 1:4 elements ordered column-first
+# 1) create object 'matr' containing a 2x2 matrix with 1:4 elements ordered column-first
 # matr <- makeCacheMatrix(matrix(1:4, nrow=2, ncol=2))
-# 2) check if matr indeed contains a 2x2 matrix
+# 2) check if 'matr' indeed contains a 2x2 matrix
 # matr$get()
-# 3) compute inverse of matr with cacheSolve; prints the inverse
+# 3) compute inverse of 'matr' with cacheSolve; prints the inverse
 # cacheSolve(matr)
-# 4) execute cacheSolve again on matr to see if it prints "getting cached data"
+# 4) execute cacheSolve again on 'matr' to see if it prints "getting cached data"
 # cacheSolve(matr)
 
 ## Write a short comment describing this function
-
+# returns list with four functions:
+# - set() sets the matrix 'x'
+# - get() returns matrix 'x'
+# - setinv() sets the inverse 'i' of matrix 'x'
+#   NB can be set efficiently with cachSolve()
+# - getinv() returns the inverse 'i' of matrix 'x'
 makeCacheMatrix <- function(x = matrix()) {
   i <- NULL
   set <- function(y) {
@@ -35,9 +40,16 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 ## Write a short comment describing this function
-
+# returns a matrix that is the inverse of 'x'
+# 53) attempts to get inverse 'i' from 'x'
+# 54) checks if 'i' already exists (is not null); 
+#    if so, then 55-56) print "getting cached data", return 'í' and exit function
+#    if not, then 
+#       58) get the 'data' (matrix) from 'x'
+#       59) compute inverse 'i' of 'data'
+#       60) set inverse of 'x' to inverse 'i'
+#       61) print inverse 'i' and exit function
 cacheSolve <- function(x, ...) {
-  ## Return a matrix that is the inverse of 'x'
   i <- x$getinv()
   if(!is.null(i)) {
     message("getting cached data")
